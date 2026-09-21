0472eb11dc916d1da85e36cd79f1e057ed9de61d
Bing <mail.bing@qq.com>
2026-09-21 09:07:42 +0800

    feat: 关于页添加作者与仓库地址(点击打开 GitHub)

--- src/EasyWin/Views/AboutPage.xaml.cs
+++ src/EasyWin/Views/AboutPage.xaml.cs
@@ -1,11 +1,23 @@
-using System.Windows.Controls;
+using System.Diagnostics;
+using System.Windows.Navigation;
 
 namespace EasyWin.Views;
 
-public partial class AboutPage : Page
+public partial class AboutPage : System.Windows.Controls.Page
 {
     public AboutPage()
     {
         InitializeComponent();
+    }
+
+    /// <summary>点击仓库超链接,用默认浏览器打开。</summary>
+    private void OnRepoNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = e.Uri.AbsoluteUri,
+            UseShellExecute = true,
+        });
+        e.Handled = true;
     }
 }
